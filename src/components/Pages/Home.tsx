0d3a6f25a9d63758d04b24acf6c9c23c56c0e103
souravthakur1992 <souravthakur1992@gmail.com>
2023-11-25 15:05:54 +0100

    Update Home.tsx

--- src/components/Pages/Home.tsx
+++ src/components/Pages/Home.tsx
@@ -7,7 +7,7 @@ function Home(){
     return(
         <Atoms.Div display='flex' flexDirection='column' width="100vw" height="100vh">
             <Template.Header />
-            <Atoms.Div display='flex' justifyContent='center' alignItems='center' width="100%" height="calc(100% - 77px)">
+            <Atoms.Div display='flex' padding="32px" justifyContent='center' alignItems='center' width="100%" height="calc(100% - 77px)">
                 <Organisms.Home />
             </Atoms.Div>
         </Atoms.Div>
