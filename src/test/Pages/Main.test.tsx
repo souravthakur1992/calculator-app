005aa30c8e62bf69b99f0375e50705bd0b41df70
souravthakur1992 <souravthakur1992@gmail.com>
2023-12-13 07:25:03 +0100

    test case added
    Snapshot testing

--- src/test/Pages/Main.test.tsx
+++ src/test/Pages/Main.test.tsx
@@ -1,9 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import { Home } from 'components/Pages';
-
+const renderer = require('react-test-renderer');
 test('render Main Page', () => {
     render(<Home />);
 
-    const h1Tag = screen.getByText(/this is main page/i);
+    const h1Tag = screen.getByText(/CALCULATOR REACT APP/i);
     expect(h1Tag).toBeInTheDocument();
 })
+describe("Snapshot test for Home page", () => {
+    it("Matches DOM Snapshot", () => {
+        const domTree = renderer.create(<Home />).toJSON();
+        expect(domTree).toMatchSnapshot();
+    });
+});
